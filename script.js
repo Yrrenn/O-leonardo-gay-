d35abcd149902ccb8dabcff7c2a52798ac1f1120
Yrrenn <akiradaeo96@gmail.com>
2026-07-20 16:03:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const btnSim = document.querySelector('#btnSim');
-const btnNao = document.querySelector('#btnNao');
+    
+  function fuja(){
+    var bataoNao = document.getElementById("Nao");
+    var x = Math.floor(Math.random() * window.innerWidth);
+    var y = Math.floor(Math.random() * window.innerHeight);
+    bataoNao.style.position = "absolute";
+    bataoNao.style.left = x + "px";
+    bataoNao.style.top = y + "px";
 
-    btnNao.addEventListener('click', function(){
 
-        alert('Para, tu é que eu sei! esse queima!');
+  }
+    
+    
+    const btnSim = document.querySelector('#Sim');
+    const btnNao = document.querySelector('#Nao');
 
+        btnNao.addEventListener('click', function(){
+
+            alert('Para, tu é que eu sei! esse queima!');
+
+        });
+
+        btnSim.addEventListener('click', function (){
+            alert('Sabia kkk!');
     });
-
-    btnSim.addEventListener('click', function (){
-        alert('Sabia kkk!');
-});
